4f41339143e57fa77f91ebbd989d7bf1dce9a9b9
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-01-24 01:44:06 +0300

    first react task ...to cont

---
+++ src/Section1.js
@@ -0,0 +1,35 @@
+
+import React from "react" ;
+import "./Section1.css";
+import "./index.css";
+
+function Section1(){
+
+    return(
+
+        <div className="Section1">
+
+           <div className="container">
+
+                    <h1>We've got what you need!</h1>
+
+                    <span></span>
+
+                    <p>
+                        Start Bootstrap has everything you need to get your new website up
+                        and running in no time! Choose one of our open source, free to download,
+                        and easy to use themes! No strings attached!
+                    </p>
+
+                    <button>GET STARTED!</button>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+
+export default Section1;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Section1 from "./Section1" ;
+import "./index.css";
+
+
+ReactDOM.render( <Section1 />  , document.getElementById("root"));
+
