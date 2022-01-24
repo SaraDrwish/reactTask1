d6ed9db911262eff5e9b479909a2bcbad87f2f94
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-01-25 00:48:49 +0300

    final

--- src/Section2.js
+++ src/Section2.js
@@ -13,10 +13,43 @@ import Section2Icons from "./Section2Icons"
                <span> </span>
 
                 <div className="iconsContainer">
+
+
+                        <Section2Icons title="Sturdy Themes" 
+                         parag="Our themes are updated regularly to keep them bug free!" 
+                          icon="far fa-heart" />
+
+
+
+                        <Section2Icons title="Up to Date" 
+                         parag="All dependencies are kept current to keep things fresh." 
+                          icon="fas fa-laptop" /> 
+
+                          <Section2Icons title="Ready to Publish" 
+                         parag="You can use this design as is, or you can make changes!" 
+                          icon="far fa-gem" />
+
+                          <Section2Icons title="Made with Love" 
+                         parag="Is it really open source if it's not made with love?" 
+                          icon="far fa-heart" /> 
+
+
+
+                        {/* <i class="fas fa-laptop"></i> */}
+
+                        {/* <i class="fas fa-globe"></i> */}
+
+                        {/* <i className="far fa-gem"> */}
+
+                        {/* <i class="far fa-heart"></i> */}
+                     
+
+                        {/* <Section2Icons/>
                         <Section2Icons/>
                         <Section2Icons/>
-                        <Section2Icons/>
-                        <Section2Icons/>
+                        <Section2Icons/> */}
+
+
                 </div>
 
 
--- src/Section2Icons.js
+++ src/Section2Icons.js
@@ -1,20 +1,31 @@
 import React from "react";
 import "./Section2Icons.css" ;
 
-function Section2Icons(){
+function Section2Icons(props){
     return ( 
 
        <div className="Section2Icons">
          
-         <div className="container">
-               <i className="far fa-gem"> </i>
-               <h2>Sturdy Themes</h2>
-               <p>Our themes are updated regularly to keep them bug free!</p>
-         </div>
-            
+            <div className="container">
+            {/* <i className="far fa-gem"> </i> */}
+
+
+            {/* <i className="far fa-gem"> </i>
+                  <h2>Sturdy Themes</h2>
+                  <p>Our themes are updated regularly to keep them bug free!</p>
+            </div> */}
+                
+
+                  <i className= { props.icon }>  </i>
+                  <h2> { props.title } </h2>
+                  <p> { props.parag } </p>
+
+
+              </div>
 
 
        </div>
+
     );
 }
 
